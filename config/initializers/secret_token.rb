8e8afb0444d424e9aaaf06d63e483a484a3ec1b9
retablismo <retablismo@yahoo.es>
2011-05-11 18:09:30 -0400

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zipiandzape::Application.config.secret_token = '90090b3b1a6c3389a36176ad275e296fdcc7114135910b5c6cf24150a94ab921c480c05abe284bafeeeba19394f868b284bd7a56a52edb98a04d335ac85be0f5'
